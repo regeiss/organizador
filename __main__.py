af964c868aed6370af846513bb8a0dc886018eaf
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-09-08 17:58:23 -0300

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+import utils
+
+def main():
+
+    arquivos = os.listdir()
+    for diretorio in ['documentos', 'planilhas']:
+        utils.make_dir(diretorio)
+    utils.move_file(arquivos)
+
+if __name__ == "__main__":
+    main() 
