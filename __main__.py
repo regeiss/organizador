315a6e870f724ee81ed0bba9d30a71b11375b3d1
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-09-09 09:23:27 -0300

    Update __main__.py
    Incluido header de comentários

--- __main__.py
+++ __main__.py
@@ -1,3 +1,16 @@
+#===========================================================================================================================================
+# -*- coding: utf-8 -*-  
+# Arquivo: __main__.py
+# Versão: 1.0
+# Última alteração: 09/09/2002 
+# Propósito: programa main projeto1
+# Autor: Roberto Edgar Geiss 
+# Copyright:  
+# Produto:  
+# Observacoes: projeto1
+# Parametros: 
+# Detalhes especificos: Curso extensão UFRGS - Introdução ao Python
+#===========================================================================================================================================
 import os
 import shutil
 import utils
@@ -11,3 +24,4 @@ def main():
 
 if __name__ == "__main__":
     main() 
+# eof
